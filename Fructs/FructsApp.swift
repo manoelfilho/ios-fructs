0ccb8dac611e0817e98e35d559351655505f59da
manoelfilho <manoel.tsf@gmail.com>
2021-04-12 00:03:36 -0300

    Initial Commit

---
+++ Fructs/FructsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructsApp.swift
+//  Fructs
+//
+//  Created by Manoel Filho on 12/04/21.
+//
+
+import SwiftUI
+
+@main
+struct FructsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
